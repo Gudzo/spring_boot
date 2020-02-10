064afa6050c19b76eeb99bf1fd6a856749b0c1f2
Filip Gudjevski <filipgudjevski@Filip-Gudjevski.local>
2020-02-10 11:46:02 +0100

    removed connection

--- src/main/java/com/project/showCase/jdbc/PostgreSQLConnection.java
+++ src/main/java/com/project/showCase/jdbc/PostgreSQLConnection.java
@@ -1,101 +1,101 @@
-package com.project.showCase.jdbc;
-
-import com.project.showCase.models.Product;
-
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-
-public class PostgreSQLConnection {
-
-    private static PostgreSQLConnection connection;
-    private static Connection con;
-
-    public PostgreSQLConnection() {
-        try {
-            Class.forName("org.postgresql.Driver").newInstance();
-            con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "bloodDonation");
-            System.out.println("Connected to PostgreSQLConnection database!");
-        } catch (SQLException | ClassNotFoundException | InstantiationException | IllegalAccessException e) {
-            System.out.println("Connection to PostgreSQL failed!");
-        }
-    }
-
-    public static PostgreSQLConnection getInstance() {
-        if (connection == null) {
-            connection = new PostgreSQLConnection();
-        }
-        return connection;
-    }
-
-    public static ResultSet executeQuery(String query) throws SQLException {
-        if (con == null) {
-            PostgreSQLConnection.getInstance();
-        }
-        Statement statement = con.createStatement();
-        return statement.executeQuery(query);
-    }
-
-    public static Integer executeQueryPreparedStm(String query) throws SQLException {
-        if (con == null) {
-            PostgreSQLConnection.getInstance();
-        }
-        PreparedStatement preparedStatement = con.prepareStatement(query);
-
-        return preparedStatement.executeUpdate();
-    }
-
-    public static List<Product> getProducts() throws SQLException {
-        String query = "select * from product";
-        ResultSet resultSet = executeQuery(query);
-        List<Product> products = new ArrayList<>();
-        while (resultSet.next()) {
-            Product product = new Product();
-            product.setId(resultSet.getInt("id"));
-            product.setName(resultSet.getString("name"));
-            product.setDescription(resultSet.getString("description"));
-            product.setPrice(resultSet.getDouble("price"));
-            product.setSeller(resultSet.getString("seller"));
-            products.add(product);
-        }
-        return products;
-    }
-
-    public static Product getProductById(Integer id) throws SQLException {
-        String query = "select * from product where id = "+ id + "";
-        ResultSet resultSet = executeQuery(query);
-        Product product = new Product();
-        if (resultSet.next()) {
-            product.setId(resultSet.getInt("id"));
-            product.setName(resultSet.getString("name"));
-            product.setDescription(resultSet.getString("description"));
-            product.setPrice(resultSet.getDouble("price"));
-            product.setSeller(resultSet.getString("seller"));
-        }
-        return product;
-    }
-
-    public static String addProduct(Product product) throws SQLException {
-        String query = "insert into product(name, description, price, seller) values('" + product.getName() + "', '"
-                + product.getDescription() + "', " + product.getPrice() + ", '" + product.getSeller() + "')";
-        executeQueryPreparedStm(query);
-        return "Product added!";
-    }
-
-    public static String updateProduct(Product product) throws SQLException {
-        String query = "update product set name = '" + product.getName() + "' where id = '" + product.getId() + "'";
-        executeQueryPreparedStm(query);
-        return "Product updated!";
-    }
-
-    public static String deleteProduct(Integer id) throws SQLException {
-        String query = "delete from product where id = '" + id + "'";
-        executeQueryPreparedStm(query);
-        return "Product deleted!";
-    }
-}
-
-
-
-
-
+//package com.project.showCase.jdbc;
+//
+//import com.project.showCase.models.Product;
+//
+//import java.sql.*;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class PostgreSQLConnection {
+//
+//    private static PostgreSQLConnection connection;
+//    private static Connection con;
+//
+//    public PostgreSQLConnection() {
+//        try {
+//            Class.forName("org.postgresql.Driver").newInstance();
+//            con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "bloodDonation");
+//            System.out.println("Connected to PostgreSQLConnection database!");
+//        } catch (SQLException | ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+//            System.out.println("Connection to PostgreSQL failed!");
+//        }
+//    }
+//
+//    public static PostgreSQLConnection getInstance() {
+//        if (connection == null) {
+//            connection = new PostgreSQLConnection();
+//        }
+//        return connection;
+//    }
+//
+//    public static ResultSet executeQuery(String query) throws SQLException {
+//        if (con == null) {
+//            PostgreSQLConnection.getInstance();
+//        }
+//        Statement statement = con.createStatement();
+//        return statement.executeQuery(query);
+//    }
+//
+//    public static Integer executeQueryPreparedStm(String query) throws SQLException {
+//        if (con == null) {
+//            PostgreSQLConnection.getInstance();
+//        }
+//        PreparedStatement preparedStatement = con.prepareStatement(query);
+//
+//        return preparedStatement.executeUpdate();
+//    }
+//
+//    public static List<Product> getProducts() throws SQLException {
+//        String query = "select * from product";
+//        ResultSet resultSet = executeQuery(query);
+//        List<Product> products = new ArrayList<>();
+//        while (resultSet.next()) {
+//            Product product = new Product();
+//            product.setId(resultSet.getInt("id"));
+//            product.setName(resultSet.getString("name"));
+//            product.setDescription(resultSet.getString("description"));
+//            product.setPrice(resultSet.getDouble("price"));
+//            product.setSeller(resultSet.getString("seller"));
+//            products.add(product);
+//        }
+//        return products;
+//    }
+//
+//    public static Product getProductById(Integer id) throws SQLException {
+//        String query = "select * from product where id = "+ id + "";
+//        ResultSet resultSet = executeQuery(query);
+//        Product product = new Product();
+//        if (resultSet.next()) {
+//            product.setId(resultSet.getInt("id"));
+//            product.setName(resultSet.getString("name"));
+//            product.setDescription(resultSet.getString("description"));
+//            product.setPrice(resultSet.getDouble("price"));
+//            product.setSeller(resultSet.getString("seller"));
+//        }
+//        return product;
+//    }
+//
+//    public static String addProduct(Product product) throws SQLException {
+//        String query = "insert into product(name, description, price, seller) values('" + product.getName() + "', '"
+//                + product.getDescription() + "', " + product.getPrice() + ", '" + product.getSeller() + "')";
+//        executeQueryPreparedStm(query);
+//        return "Product added!";
+//    }
+//
+//    public static String updateProduct(Product product) throws SQLException {
+//        String query = "update product set name = '" + product.getName() + "' where id = '" + product.getId() + "'";
+//        executeQueryPreparedStm(query);
+//        return "Product updated!";
+//    }
+//
+//    public static String deleteProduct(Integer id) throws SQLException {
+//        String query = "delete from product where id = '" + id + "'";
+//        executeQueryPreparedStm(query);
+//        return "Product deleted!";
+//    }
+//}
+//
+//
+//
+//
+//
--- src/main/java/com/project/showCase/restControllers/ProductRestController.java
+++ src/main/java/com/project/showCase/restControllers/ProductRestController.java
@@ -1,108 +1,108 @@
-package com.project.showCase.restControllers;
-
-//import com.project.showCase.jdbc.JdbcProductRepository;
-import com.project.showCase.jdbc.PostgreSQLConnection;
-import com.project.showCase.models.Product;
-import com.project.showCase.services.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.sql.SQLException;
-import java.util.List;
-
-@RestController
-public class ProductRestController {
-
-    @Autowired
-    ProductService productService;
-    @Autowired
-//    JdbcProductRepository jdbcProductRepository;
-
+//package com.project.showCase.restControllers;
+//
+////import com.project.showCase.jdbc.JdbcProductRepository;
+//import com.project.showCase.jdbc.PostgreSQLConnection;
+//import com.project.showCase.models.Product;
+//import com.project.showCase.services.ProductService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.sql.SQLException;
+//import java.util.List;
+//
+//@RestController
+//public class ProductRestController {
+//
+//    @Autowired
+//    ProductService productService;
+//    @Autowired
+////    JdbcProductRepository jdbcProductRepository;
+//
+////    @GetMapping("/getAllProducts")
+////    public ResponseEntity<List> getProducts() {
+////        List<Product> listOfAlProducts = jdbcProductRepository.findAll();
+////        return new ResponseEntity<>(listOfAlProducts, HttpStatus.OK);
+////    }
+//
 //    @GetMapping("/getAllProducts")
-//    public ResponseEntity<List> getProducts() {
-//        List<Product> listOfAlProducts = jdbcProductRepository.findAll();
+//    public ResponseEntity<List> getProducts() throws SQLException {
+//        List<Product> listOfAlProducts = PostgreSQLConnection.getProducts();
 //        return new ResponseEntity<>(listOfAlProducts, HttpStatus.OK);
 //    }
-
-    @GetMapping("/getAllProducts")
-    public ResponseEntity<List> getProducts() throws SQLException {
-        List<Product> listOfAlProducts = PostgreSQLConnection.getProducts();
-        return new ResponseEntity<>(listOfAlProducts, HttpStatus.OK);
-    }
-
+//
+////    @GetMapping("/getProductById/{id}")
+////    public ResponseEntity<Product> getProductById(@PathVariable("id") Integer id) {
+////        Product product = jdbcProductRepository.findById(id);
+////        return new ResponseEntity<>(product, HttpStatus.OK);
+////    }
+//
 //    @GetMapping("/getProductById/{id}")
-//    public ResponseEntity<Product> getProductById(@PathVariable("id") Integer id) {
-//        Product product = jdbcProductRepository.findById(id);
+//    public ResponseEntity<Product> getProductById(@PathVariable("id") Integer id) throws SQLException {
+//        Product product = PostgreSQLConnection.getProductById(id);
 //        return new ResponseEntity<>(product, HttpStatus.OK);
 //    }
-
-    @GetMapping("/getProductById/{id}")
-    public ResponseEntity<Product> getProductById(@PathVariable("id") Integer id) throws SQLException {
-        Product product = PostgreSQLConnection.getProductById(id);
-        return new ResponseEntity<>(product, HttpStatus.OK);
-    }
-
+//
+////    @PostMapping("/addPhoneProduct")
+////    public ResponseEntity<String> addPhoneProduct(@RequestBody Product newProduct) {
+////        jdbcProductRepository.save(newProduct);
+////        return new ResponseEntity<>("Product has been added!", HttpStatus.OK);
+////    }
+//
 //    @PostMapping("/addPhoneProduct")
-//    public ResponseEntity<String> addPhoneProduct(@RequestBody Product newProduct) {
-//        jdbcProductRepository.save(newProduct);
+//    public ResponseEntity<String> addPhoneProduct(@RequestBody Product newProduct) throws SQLException {
+//        PostgreSQLConnection.addProduct(newProduct);
 //        return new ResponseEntity<>("Product has been added!", HttpStatus.OK);
 //    }
-
-    @PostMapping("/addPhoneProduct")
-    public ResponseEntity<String> addPhoneProduct(@RequestBody Product newProduct) throws SQLException {
-        PostgreSQLConnection.addProduct(newProduct);
-        return new ResponseEntity<>("Product has been added!", HttpStatus.OK);
-    }
-
+//
+////    @PutMapping("/updateProduct")
+////    public ResponseEntity<String> updateProduct(@RequestBody Product updatedProduct) {
+////        jdbcProductRepository.updateProductName(updatedProduct);
+////        return new ResponseEntity<>("Product " + updatedProduct.getId() + " has been updated!", HttpStatus.OK);
+////    }
+//
 //    @PutMapping("/updateProduct")
-//    public ResponseEntity<String> updateProduct(@RequestBody Product updatedProduct) {
-//        jdbcProductRepository.updateProductName(updatedProduct);
+//    public ResponseEntity<String> updateProduct(@RequestBody Product updatedProduct) throws SQLException {
+//        PostgreSQLConnection.updateProduct(updatedProduct);
 //        return new ResponseEntity<>("Product " + updatedProduct.getId() + " has been updated!", HttpStatus.OK);
 //    }
-
-    @PutMapping("/updateProduct")
-    public ResponseEntity<String> updateProduct(@RequestBody Product updatedProduct) throws SQLException {
-        PostgreSQLConnection.updateProduct(updatedProduct);
-        return new ResponseEntity<>("Product " + updatedProduct.getId() + " has been updated!", HttpStatus.OK);
-    }
-
+//
+////    @DeleteMapping("/deleteProduct/{id}")
+////    public ResponseEntity<String> deleteProduct(@PathVariable Integer id) {
+////        jdbcProductRepository.deleteById(id);
+////        return new ResponseEntity<>("Product " + id + " has been deleted!", HttpStatus.OK);
+////    }
+//
 //    @DeleteMapping("/deleteProduct/{id}")
-//    public ResponseEntity<String> deleteProduct(@PathVariable Integer id) {
-//        jdbcProductRepository.deleteById(id);
+//    public ResponseEntity<String> deleteProduct(@PathVariable Integer id) throws SQLException {
+//        PostgreSQLConnection.deleteProduct(id);
 //        return new ResponseEntity<>("Product " + id + " has been deleted!", HttpStatus.OK);
 //    }
-
-    @DeleteMapping("/deleteProduct/{id}")
-    public ResponseEntity<String> deleteProduct(@PathVariable Integer id) throws SQLException {
-        PostgreSQLConnection.deleteProduct(id);
-        return new ResponseEntity<>("Product " + id + " has been deleted!", HttpStatus.OK);
-    }
-
-
-
-//    //These were used as templates with no DB connection
-//    @GetMapping("/getProducts")
-//    public ResponseEntity<List> getAllProducts() {
-//        List<Product> listOfProducts = productService.getAllProducts();
-//        return new ResponseEntity<>(listOfProducts, HttpStatus.OK);
-//    }
 //
-//    @GetMapping("/getProduct/{id}")
-//    public ResponseEntity<Product> getProduct(@PathVariable("id") String productId) {
-//        Product product = productService.getProductById(productId);
-//        if (product == null) {
-//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//        } else {
-//            return new ResponseEntity<>(product, HttpStatus.OK);
-//        }
-//    }
 //
-//    @PostMapping("/addProduct")
-//    public Product addProduct(@RequestBody Product newProduct) {
-//        System.out.println(newProduct.toString());
-//        return productService.addProduct(newProduct);
-//    }
-
-}
+//
+////    //These were used as templates with no DB connection
+////    @GetMapping("/getProducts")
+////    public ResponseEntity<List> getAllProducts() {
+////        List<Product> listOfProducts = productService.getAllProducts();
+////        return new ResponseEntity<>(listOfProducts, HttpStatus.OK);
+////    }
+////
+////    @GetMapping("/getProduct/{id}")
+////    public ResponseEntity<Product> getProduct(@PathVariable("id") String productId) {
+////        Product product = productService.getProductById(productId);
+////        if (product == null) {
+////            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+////        } else {
+////            return new ResponseEntity<>(product, HttpStatus.OK);
+////        }
+////    }
+////
+////    @PostMapping("/addProduct")
+////    public Product addProduct(@RequestBody Product newProduct) {
+////        System.out.println(newProduct.toString());
+////        return productService.addProduct(newProduct);
+////    }
+//
+//}
